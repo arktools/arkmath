122287dd698391fc378138658b9b4e479bc6456e
James Goppert <jgoppert@w7.hsl.dynalias.com>
2011-08-31 18:35:43 -0400

    Fixed navigation error.

--- src/gen_fortran/ins_dynamics_f.f90
+++ src/gen_fortran/ins_dynamics_f.f90
@@ -1,7 +1,7 @@
-f(1,1) = 0.5*(-d*(Ve*tan(L)/R+Omega*sin(L)+(d**2-c**2-b**2+a**2)*wz+(2*c*d+2*a*b)*wy+(2*b*d-2*a*c)*wx)-c*(Vn/R+(2*c*d-2*a*b)*wz+(-d**2+c**2-b**2+a**2)*wy+(2*a*d+2*b*c)*wx)-b*(-Ve/R-Omega*cos(L)+(2*b*d+2*a*c)*wz+(2*b*c-2*a*d)*wy+(-d**2-c**2+b**2+a**2)*wx))
-f(2,1) = 0.5*(c*(Ve*tan(L)/R+Omega*sin(L)+(d**2-c**2-b**2+a**2)*wz+(2*c*d+2*a*b)*wy+(2*b*d-2*a*c)*wx)-d*(Vn/R+(2*c*d-2*a*b)*wz+(-d**2+c**2-b**2+a**2)*wy+(2*a*d+2*b*c)*wx)+a*(-Ve/R-Omega*cos(L)+(2*b*d+2*a*c)*wz+(2*b*c-2*a*d)*wy+(-d**2-c**2+b**2+a**2)*wx))
-f(3,1) = 0.5*(-b*(Ve*tan(L)/R+Omega*sin(L)+(d**2-c**2-b**2+a**2)*wz+(2*c*d+2*a*b)*wy+(2*b*d-2*a*c)*wx)+a*(Vn/R+(2*c*d-2*a*b)*wz+(-d**2+c**2-b**2+a**2)*wy+(2*a*d+2*b*c)*wx)+d*(-Ve/R-Omega*cos(L)+(2*b*d+2*a*c)*wz+(2*b*c-2*a*d)*wy+(-d**2-c**2+b**2+a**2)*wx))
-f(4,1) = 0.5*(a*(Ve*tan(L)/R+Omega*sin(L)+(d**2-c**2-b**2+a**2)*wz+(2*c*d+2*a*b)*wy+(2*b*d-2*a*c)*wx)+b*(Vn/R+(2*c*d-2*a*b)*wz+(-d**2+c**2-b**2+a**2)*wy+(2*a*d+2*b*c)*wx)-c*(-Ve/R-Omega*cos(L)+(2*b*d+2*a*c)*wz+(2*b*c-2*a*d)*wy+(-d**2-c**2+b**2+a**2)*wx))
+f(1,1) = 0.5*(-d*wz-c*wy-b*wx)
+f(2,1) = 0.5*(c*wz-d*wy+a*wx)
+f(3,1) = 0.5*(-b*wz+a*wy+d*wx)
+f(4,1) = 0.5*(a*wz+b*wy-c*wx)
 f(5,1) = -Ve*(Ve*tan(L)/R+2*Omega*sin(L))+Vd*Vn/R+(2*b*d+2*a*c)*fz+(2*b*c-2*a*d)*fy+(-d**2-c**2+b**2+a**2)*fx
 f(6,1) = -Vn*(-Ve*tan(L)/R-2*Omega*sin(L))-Vd*(-Ve/R-2*Omega*cos(L))+(2*c*d-2*a*b)*fz+(-d**2+c**2-b**2+a**2)*fy+(2*a*d+2*b*c)*fx
 f(7,1) = -Ve*(Ve/R+2*Omega*cos(L))-Vn**2/R+g+(d**2-c**2-b**2+a**2)*fz+(2*c*d+2*a*b)*fy+(2*b*d-2*a*c)*fx

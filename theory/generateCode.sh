b0bdaeefc14369961f20daa0ee72370df8bfd28d
James Goppert <james.goppert@gmail.com>
2011-09-24 18:38:39 -0400

    Fixed attitude indices.

--- theory/generateCode.sh
+++ theory/generateCode.sh
@@ -290,6 +290,37 @@ cat $f90Path/ins_error_dynamics_G_vp.f90 | sed \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
 	> ${outPath}/ins_error_dynamics_G_vp.hpp
 
+cat $f90Path/ins_error_dynamics_G_att.f90 | sed \
+	-e "s/$/;/g" \
+	-e "/^.*) = 0;$/d" \
+	-e "s/a\*\*2/aa/g" -e "s/b\*\*2/bb/g" -e "s/c\*\*2/cc/g" -e "s/d\*\*2/dd/g" \
+	-e "s/a\*\*3/aaa/g" -e "s/b\*\*3/bbb/g" -e "s/c\*\*3/ccc/g" -e "s/d\*\*3/ddd/g" \
+	-e "s/cos(L)/cosL/g" -e "s/sin(L)/sinL/g" -e "s/tan(L)/tanL/g" \
+	-e "s/sec(L)/secL/g" \
+	-e "s/secL\**2/secLsecL/g" \
+	-e "s/R\*\*2/RR/g" \
+	-e "s/cosL\*\*2/cosLcosL/g" \
+	-e "s/(1,/(0,/g" -e "s/,1)/,0)/g" \
+	-e "s/(2,/(1,/g" -e "s/,2)/,1)/g" \
+	-e "s/(3,/(2,/g" -e "s/,3)/,2)/g" \
+	-e "s/(4,/(3,/g" -e "s/,4)/,3)/g" \
+	-e "s/(5,/(4,/g" -e "s/,5)/,4)/g" \
+	-e "s/(6,/(5,/g" -e "s/,6)/,5)/g" \
+	-e "s/(7,/(6,/g" -e "s/,7)/,6)/g" \
+	-e "s/(8,/(7,/g" -e "s/,8)/,7)/g" \
+	-e "s/(9,/(8,/g" -e "s/,9)/,8)/g" \
+	-e "s/(10,/(9,/g" -e "s/,10)/,9)/g" \
+	-e "s/(11,/(10,/g" -e "s/,11)/,10)/g" \
+	-e "s/(12,/(11,/g" -e "s/,12)/,11)/g" \
+	-e "s/(13,/(12,/g" -e "s/,13)/,12)/g" \
+	-e "s/(14,/(13,/g" -e "s/,14)/,13)/g" \
+	-e "s/(15,/(14,/g" -e "s/,15)/,14)/g" \
+	-e "s/(16,/(15,/g" -e "s/,16)/,15)/g" \
+	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
+	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
+	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
+	> ${outPath}/ins_error_dynamics_G_att.hpp
+
 cat $f90Path/z_mag.f90 | sed \
 	-e "s/$/;/g" \
 	-e "/^.*) = 0;$/d" \

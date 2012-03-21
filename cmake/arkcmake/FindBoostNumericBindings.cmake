717cb69587be308725c9005ada990e7c690b61fd
James Goppert <james.goppert@gmail.com>
2012-03-21 01:42:29 -0400

    Cmake cleanup.

--- cmake/arkcmake/FindBoostNumericBindings.cmake
+++ cmake/arkcmake/FindBoostNumericBindings.cmake
@@ -3,14 +3,20 @@
 #
 #  BOOSTNUMERICBINDINGS_FOUND        : library found
 #  BOOSTNUMERICBINDINGS_INCLUDE_DIRS : include directories
-#  BOOSTNUMERICBINDINGS_VERSION      : version
 
 # macros
 include(FindPackageHandleStandardArgs)
 
+set(_BOOSTNUMERICBINDINGS_EXTRA_SEARCH_PATHS
+    /usr/local
+    /opt/local
+    )
+
 # find the include directory
 find_path(_BOOSTNUMERICBINDINGS_INCLUDE_DIR
     NAMES boost/numeric/bindings/lapack/lapack.h
+    PATHS ${_BOOSTNUMERICBINDINGS_EXTRA_SEARCH_PATHS}
+    PATH_SUFFIXES include
     )
 
 # handle arguments
